e07684fce57a751cc268daa3911e23911bc38912
nihadhuseynll <huseynlinihad4400@gmail.com>
2025-06-19 12:32:25 +0400

    Initial commit

---
+++ src/main/java/com/nkcode/nksocialmedia/NkSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.nkcode.nksocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkSocialMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NkSocialMediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nkcode/nksocialmedia/NkSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkcode.nksocialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkSocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
